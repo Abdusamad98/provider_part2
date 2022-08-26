81ba384f80908759d4b185f0bce822c85c88cc60
Abdusamad98 <abdukarimovabdusamad1998@gmail.com>
2022-08-26 17:19:40 +0500

    new changes

--- lib/data_layer/repository/user_repository.dart
+++ lib/data_layer/repository/user_repository.dart
@@ -1,10 +1,20 @@
+import 'package:provider_part2/data_layer/db/cached_user_model.dart';
+import 'package:provider_part2/data_layer/db/local_database.dart';
 import 'package:provider_part2/data_layer/models/user_data.dart';
 import 'package:provider_part2/data_layer/services/api_service.dart';
 
 class UserRepository {
-  UserRepository({required this.apiService});
+  UserRepository({required ApiService apiService}) {
+    _apiService = apiService;
+  }
 
-  final ApiService apiService;
+  late ApiService _apiService;
 
-  Future<UserData> getUserData() => apiService.getUserData();
+  Future<UserData> getUserData() => _apiService.getUserData();
+
+  //-----------------------Local Users side-------------------
+
+  Future<List<CachedUser>> getCachedUsers() => LocalDataBase.getAllCachedUsers();
+  Future<CachedUser> insertUserFromApi({required UserData userData}) => LocalDataBase.insertCachedUserFromApi(userData);
+
 }
--- lib/view_models/user_view_model.dart
+++ lib/view_models/user_view_model.dart
@@ -1,8 +1,32 @@
 import 'package:flutter/foundation.dart';
+import 'package:provider_part2/data_layer/db/cached_user_model.dart';
+import 'package:provider_part2/data_layer/models/user_data.dart';
 import 'package:provider_part2/data_layer/repository/user_repository.dart';
 
 class UserViewModel extends ChangeNotifier {
   UserViewModel({required this.userRepository});
 
   final UserRepository userRepository;
+
+  List<CachedUser> cachedUsers = [];
+  UserData? userData;
+  bool isLoading = false;
+
+  void getCachedUsers() async {
+    isLoading = true;
+    notifyListeners();
+    cachedUsers = await userRepository.getCachedUsers();
+    isLoading = false;
+    notifyListeners();
+  }
+
+  void getUserData() async {
+    isLoading = true;
+    notifyListeners();
+    userData = await userRepository.getUserData();
+    await userRepository.insertUserFromApi(userData: userData!);
+    cachedUsers = await userRepository.getCachedUsers();
+    isLoading = false;
+    notifyListeners();
+  }
 }
