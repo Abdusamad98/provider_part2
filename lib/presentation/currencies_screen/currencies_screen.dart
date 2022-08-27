d819caf049eebaefcd7c3bc1647c1905e070f1e2
Abdusamad98 <abdukarimovabdusamad1998@gmail.com>
2022-08-27 06:18:51 +0500

    new changes

--- lib/presentation/currencies_screen/currencies_screen.dart
+++ lib/presentation/currencies_screen/currencies_screen.dart
@@ -4,12 +4,30 @@ import 'package:provider_part2/data_layer/models/currency_item.dart';
 import 'package:provider_part2/presentation/user_screen/users_screen.dart';
 import 'package:provider_part2/view_models/currencies_view_model.dart';
 
-class CurrenciesScreen extends StatelessWidget {
+class CurrenciesScreen extends StatefulWidget {
   const CurrenciesScreen({Key? key}) : super(key: key);
 
   @override
+  State<CurrenciesScreen> createState() => _CurrenciesScreenState();
+}
+
+class _CurrenciesScreenState extends State<CurrenciesScreen> {
+
+  @override
+  void initState() {
+    _init();
+    super.initState();
+
+  }
+
+  void _init(){
+    Future.delayed(Duration.zero, () async {
+      Provider.of<CurrencyViewModel>(context, listen: false).fetchCurrencyList();
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    Provider.of<CurrencyViewModel>(context, listen: false).fetchCurrencyList();
     return Scaffold(
       appBar: AppBar(
         title: const Text("Home Screen"),
